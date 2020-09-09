4572d4e5426a59c3f69bba5cc37870d80997afd6
usefulparade <breadclip.cooperative@gmail.com>
2020-09-09 01:13:35 -0400

    added thesis statement overlay

--- js/sam.js
+++ js/sam.js
@@ -6,11 +6,14 @@ var body;
 var bg;
 var bgHeightDiff;
 
+var thesisToggled;
+
 function setup(){
   createCanvas(0,0);
   body = document.querySelector(".main-column");
   bg = document.querySelector(".bg");
   window.onscroll = parallax;
+  thesisToggled = false;
 }
 
 function draw(){
@@ -35,4 +38,20 @@ function parallax(){
   // console.log("height: " + height + ", scrollPos: " + scrollPos + ", percent: " + scrollPercent);
   console.log("bg height diff: " + (bg.clientHeight - window.innerHeight));
   console.log("top offset: " + lerp(0, bgHeightDiff, scrollPercent));
+}
+
+function toggleThesis(){
+  var thesis = document.querySelector('.thesis');
+  if (!thesisToggled){
+    if (windowWidth > 720){
+      thesis.style.maxHeight = "600px";
+      
+    } else {
+      thesis.style.maxHeight = "300px";
+    }
+    thesisToggled = true;
+  } else {
+    thesis.style.maxHeight = "0px";
+    thesisToggled = false;
+  }
 }
